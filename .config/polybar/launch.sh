d632edbffb7ca9960a6fb605ae599e754ae88f8c
danishprakash <grafitykoncept@gmail.com>
2022-05-17 10:15:39 +0530

    .config/polybar: run for all connected monitors
    Signed-off-by: danishprakash <grafitykoncept@gmail.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,14 @@ killall -q polybar
 
 while pgrep -x polybar >//dev/null; do sleep 1; done
 
-polybar top & disown
+# launch polybar for connected monitors
+# https://github.com/polybar/polybar/issues/763#issuecomment-331604987
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload top &
+    done
+else
+    polybar --reload top &
+fi
 
 echo "bars launched"
