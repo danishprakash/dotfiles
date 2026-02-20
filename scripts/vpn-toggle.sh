3d7dd02528ef12c86b569a3b05fd499ef12daf4b
Danish Prakash <contact@danishpraka.sh>
2026-02-20 19:30:24 +0530

    Remove scripts directory (will be replaced with submodule)

--- scripts/vpn-toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-CONNECTION_NAME=SUSE-Engineering
-
-if [[ -n $(nmcli connection show $CONNECTION_NAME | grep "VPN connected") ]]; then
-  nmcli connection down $CONNECTION_NAME;
-else
-  nmcli connection up $CONNECTION_NAME;
-fi
